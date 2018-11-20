479cf3cb5b5cee83216fdb727e47deff80e7b353
Mathew Allington <funkymallington@gmail.com>
2018-11-20 12:30:02 +0000

    Libraries and transmitter code

---
+++ Drone_Drop_System/Transmitter Testing/Transmitter/Transmitter.ino
@@ -0,0 +1,36 @@
+ #include <SPI.h>
+    #include "nRF24L01.h"
+    #include "RF24.h"
+ 
+    RF24 radio(9,10);
+ 
+    const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+    unsigned long Command ;
+    void setup()
+    {
+    Serial.begin(57600);
+ pinMode(4,INPUT);
+    radio.begin();
+    radio.setRetries(15,15);
+    radio.openReadingPipe(1,pipes[1]);
+    radio.startListening();
+    radio.printDetails();
+    radio.openWritingPipe(pipes[0]);
+    radio.openReadingPipe(1,pipes[1]);
+    radio.stopListening();
+    }
+ 
+    void loop(void)
+    {
+    radio.stopListening();
+    if (digitalRead(4)==1){
+    Command=1;
+    }
+    else{
+      Command=2;
+     }
+      radio.write( &Command, sizeof(unsigned long) );
+    radio.startListening();
+
+    delay(1000);
+    }
